217a17cde815970b63b62e4d3b737fe7be6b92bb
dhikannn <dhikan08@gmail.com>
2026-01-02 09:13:17 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -69,31 +69,22 @@ const isOriginAllowed = (origin) => {
     return allowedPatterns.some(pattern => pattern.test(origin));
 };
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (isOriginAllowed(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('CORS Blocked'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    credentials: true
-}));
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (isOriginAllowed(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-Guest-ID');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+    }
 
-app.options('*', cors({
-    origin: (origin, callback) => {
-        if (isOriginAllowed(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('CORS Blocked'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    credentials: true
-}));
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+
 
 const csrfCheck = (req, res, next) => {
     if (['POST', 'PUT', 'DELETE'].includes(req.method)) {
