1c50f3452b77123ad69e87d8e8caba6250a83ac4
dhikannn <dhikan08@gmail.com>
2026-01-02 09:21:54 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -71,7 +71,7 @@ const isOriginAllowed = (origin) => {
 
 app.use((req, res, next) => {
     const origin = req.headers.origin;
-    if (isOriginAllowed(origin)) {
+    if (origin && isOriginAllowed(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-Guest-ID');
